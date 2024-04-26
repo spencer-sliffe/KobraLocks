93ebd13f48779fb7e698fcf738cee1f3f00b68c8
Spencer Sliffe <spenceronw@ku.edu>
2024-04-25 22:27:55 -0500

    Update App.test.js

--- frontend-web/src/App.test.js
+++ frontend-web/src/App.test.js
@@ -3,15 +3,15 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 // Test to check if the App component renders
-test('renders App component', () => {
+test('renders App component', async () => {
   render(<App />);
-  expect(screen.getByText(/Learn More/i)).toBeInTheDocument(); // Confirm that the "Learn More" link is rendered
+  const linkElement = await screen.findByText(/Learn More/i);  // Using findByText to wait for the element to appear
+  expect(linkElement).toBeInTheDocument();
 });
 
 // Test to verify the typewriter effect starts typing and checks for partial text
 test('starts typing the header', async () => {
   render(<App />);
-  // Wait for the text part to appear using findByText which includes its own waiting mechanism
-  const textElement = await screen.findByText(/Welc/i, {timeout: 3000}); // Adjust timeout according to your averageSpeed
-  expect(textElement).toBeInTheDocument();
+  const partialTextElement = await screen.findByText(/Welc/i, {timeout: 3000}); // Wait for the text part to appear
+  expect(partialTextElement).toBeInTheDocument();
 });
