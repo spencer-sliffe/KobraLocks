5f28ace4886dc634e4e2c99f99d5091741c28cdf
Spencer Sliffe <spenceronw@ku.edu>
2024-04-25 21:08:09 -0500

    trying again

--- frontend-web/src/App.test.js
+++ frontend-web/src/App.test.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import Welcome from './Welcome'; // Adjust the import path according to your project structure
+import { render, screen, waitFor } from '@testing-library/react';
+import App from './App';
 
-describe('Welcome Component', () => {
-  test('renders the welcome message', () => {
-    render(<Welcome />);
-    const welcomeElement = screen.getByText(/welcome/i); // This regex assumes your message includes 'welcome'
-    expect(welcomeElement).toBeInTheDocument();
+// Test to check if the App component renders
+test('renders App component', () => {
+  render(<App />);
+  expect(screen.getByText(/learn more/i)).toBeInTheDocument();
+});
+
+// Test to verify typewriter effect starts typing
+test('starts typing the header', async () => {
+  render(<App />);
+  await waitFor(() => expect(screen.getByText(/Welcome to KobraLocks/i)).toBeInTheDocument(), {
+    timeout: 3000 // Adjust timeout according to your averageSpeed
   });
 });
