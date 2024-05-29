6878dc04e47f4d99182e2deee2a67593c0505c43
Spencer Sliffe <spenceronw@ku.edu>
2024-05-29 10:52:20 -0500

    trying again

--- frontend-web/src/components/Navbar/NavbarElements.js
+++ frontend-web/src/components/Navbar/NavbarElements.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import { NavLink as Link } from 'react-router-dom';
-import { FaBars } from 'react-icons/fa';
 
 export const Nav = styled.nav`
   background: transparent;
